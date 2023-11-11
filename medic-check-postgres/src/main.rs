def7392699251da8d165ab71c5769ee7348dfbc8
Eric Saxby <sax@livinginthepast.org>
2023-11-11 11:36:46 -0800

    Remove redundant return value

--- medic-check-postgres/src/main.rs
+++ medic-check-postgres/src/main.rs
@@ -1,15 +1,13 @@
 use medic_check_postgres::cli::{CliArgs, Command};
-use medic_lib::CheckResult::{self, CheckOk};
+use medic_lib::CheckResult;
 
 fn main() -> CheckResult {
     let cli = CliArgs::new();
 
     match cli.command {
-        Command::DataDirectory(args) => medic_check_postgres::data_directory(args)?,
-        Command::DatabaseExists(args) => medic_check_postgres::database_exists(args)?,
-        Command::RoleExists(args) => medic_check_postgres::role_exists(args)?,
-        Command::Running(args) => medic_check_postgres::check_running(args)?,
+        Command::DataDirectory(args) => medic_check_postgres::data_directory(args),
+        Command::DatabaseExists(args) => medic_check_postgres::database_exists(args),
+        Command::RoleExists(args) => medic_check_postgres::role_exists(args),
+        Command::Running(args) => medic_check_postgres::check_running(args),
     }
-
-    CheckOk
 }
